efd75232ca0457081bc58998832592c0030adb7f
Edward Gao <edward.gao@airbyte.io>
2024-02-16 11:05:47 -0800

    switch to kotlin

---
+++ airbyte-cdk/java/airbyte-cdk/typing-deduping/src/main/java/io/airbyte/integrations/base/destination/typing_deduping/DestinationInitialState.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+ */
+package io.airbyte.integrations.base.destination.typing_deduping
+
+import java.util.*
+
+@JvmRecord
+data class DestinationInitialState<TableDefinition, DestinationState>(val streamConfig: StreamConfig,
+                                                                      val finalTableDefinition: Optional<TableDefinition>,
+                                                                      val initialRawTableState: InitialRawTableState,
+                                                                      val isSchemaMismatch: Boolean,
+                                                                      val isFinalTableEmpty: Boolean,
+                                                                      val destinationState: DestinationState) {
+  /**
+   * Utility method for java interop. Kotlin files should just use the [copy] method directly.
+   */
+  fun withDestinationState(updatedState: DestinationState) = copy(destinationState = updatedState)
+}
